00b0552529e23420d3a4d7ffe2cf9d6211f370d0
mcumm64 <mcumm64@gmail.com>
2019-05-04 02:02:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,9 +3,17 @@ def instantiate_new_array
   new_array
 end
 
+<<<<<<< HEAD
 def array_with_two_elements
   array = ["A", "B"]
   array
+=======
+def array_with_two_elements(ele1, ele2)
+  two_ele_array = []
+  two_ele_array[0] = ele1
+  two_ele_array[1] = ele2
+  two_ele_array
+>>>>>>> 0c18b05524d7238e7f892482075e7aae5f068d05
 end
 
 def first_element(array)
